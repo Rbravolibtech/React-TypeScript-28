7b55adbde6ad227c8df5ae4a30bcbdcd645a1679
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-12-04 15:31:20 -0700

    working with props with typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import Todos from './components/Todos';
+
+function App() {
+  return (
+    <div>
+      <Todos items={["Learn React", "learn typeScript"]} />
+    </div>
+  );
+}
+
+export default App;
