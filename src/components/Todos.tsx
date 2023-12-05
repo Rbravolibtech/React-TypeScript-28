84fb787b3dfe26a39d855d6c297ef6ee861ff5b3
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-12-05 11:56:36 -0700

    added code to handle to remove todo list

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,18 +1,17 @@
+// Todos.tsx
 import React from "react";
-
 import TodoItem from "./TodoItem";
-import Todo from "../models/todo"
+import Todo from "../models/todo";
+import classes from "./Todos.module.css";
 
-import classes from "./Todos.module.css"
-
-const Todos: React.FC<{items: Todo[]}> = (props) => {
-return ( 
-<ul className={classes.todos}>
-  {props.items.map((item) => ( 
-<TodoItem key={item.id} text={item.text}/>
-  ))}
-</ul>
-);
+const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (props) => {
+  return (
+    <ul className={classes.todos}>
+      {props.items.map((item) => (
+        <TodoItem key={item.id} text={item.text} onRemoveTodo={() => props.onRemoveTodo(item.id)} />
+      ))}
+    </ul>
+  );
 };
 
 export default Todos;
